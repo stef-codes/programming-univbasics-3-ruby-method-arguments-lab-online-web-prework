9c040f16ef97953d1dd257e3a7bab01aed2ebacd
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-11 07:08:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,13 +7,10 @@ def introduction_with_language(name,language)
   I am learning to program in #{language}."
 end
 
-=begin
-def introduction_with_language(name,language)
-  language = "Ruby"
+def introduction_with_language(name,language = "Ruby")
   puts "Hi, my name is #{name} and 
   I am learning to program in #{language}."
 end
-=end
 
 
 
